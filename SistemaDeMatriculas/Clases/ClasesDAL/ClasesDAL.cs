b0df1b1b0d59d112619fd66572d6d0c5b1813661
Desger Madrigal <moi123madrigal@gmail.com>
2023-07-22 10:08:16 -0600

    update

--- SistemaDeMatriculas/Clases/ClasesDAL/ClasesDAL.cs
+++ SistemaDeMatriculas/Clases/ClasesDAL/ClasesDAL.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
 
 namespace SistemaDeMatriculas.Clases
 {
@@ -19,6 +17,9 @@ namespace SistemaDeMatriculas.Clases
         public string Apellido { get; set; }
         public string CorreoElectronico { get; set; }
         public DateTime FechaContratacion { get; set; }
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; }
+        public bool Activo { get; set; }
     }
     public class Estudiante
     {
@@ -49,10 +50,85 @@ namespace SistemaDeMatriculas.Clases
     }
     public class Oferta
     {
-        public string CodigoOferta { get; set; }
-        public string Nombre { get; set; }
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Descripcion { get; set; }
         public DateTime FechaInicio { get; set; }
         public DateTime FechaFin { get; set; }
     }
+
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string NombreUsuario { get; set; }
+        public string ContraseñaHash { get; set; }
+        public string Salt { get; set; }
+        public bool EsFuncionario { get; set; }
+        public bool Activo { get; set; }
+
+        public Usuario()
+        {
+            // Constructor vacío necesario para la serialización/deserialización de objetos
+        }
+
+        public Usuario(string nombreUsuario, string contraseña, bool esFuncionario)
+        {
+            NombreUsuario = nombreUsuario;
+            EsFuncionario = esFuncionario;
+
+            // Generar una nueva contraseña y salt, y calcular el hash
+            Salt = GenerarSalAleatoria();
+            string contraseñaConSal = contraseña + Salt;
+            ContraseñaHash = CalcularSHA256Hash(contraseñaConSal);
+
+            Activo = true;
+        }
+
+        private string GenerarSalAleatoria()
+        {
+            byte[] randomBytes = new byte[32]; // 32 bytes = 256 bits
+            using (System.Security.Cryptography.RandomNumberGenerator rng = System.Security.Cryptography.RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(randomBytes);
+            }
+            return Convert.ToBase64String(randomBytes);
+        }
+
+        private string CalcularSHA256Hash(string input)
+        {
+            using (System.Security.Cryptography.SHA256 sha256 = System.Security.Cryptography.SHA256.Create())
+            {
+                byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+                return Convert.ToBase64String(hashBytes);
+            }
+        }
+    }
+
+    public class MateriaEstudiante
+    {
+        public int Id { get; set; }
+        public int IdMateria { get; set; }
+        public int IdEstudiante { get; set; }
+        public string Condicion { get; set; } // Puede ser "Aprobado" o "Reprobado"
+        public int Periodo { get; set; } // Puede ser el número del cuatrimestre, bimestre, etc.
+        public int Año { get; set; }
+        public string CodigoMateria { get; set; }
+        public string NombreMateria { get; set; }
+
+        public MateriaEstudiante()
+        {
+            // Constructor vacío necesario para la serialización/deserialización de objetos
+        }
+
+        public MateriaEstudiante(int idMateria, int idEstudiante, string condicion, int periodo, int año)
+        {
+            IdMateria = idMateria;
+            IdEstudiante = idEstudiante;
+            Condicion = condicion;
+            Periodo = periodo;
+            Año = año;
+        }
+    }
 }
-}
+
