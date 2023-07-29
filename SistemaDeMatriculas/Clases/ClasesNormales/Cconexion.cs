e309878eefe25e92ff01851c7fa7d0017d8e27cd
Desger Madrigal <moi123madrigal@gmail.com>
2023-07-28 20:03:04 -0600

    update(version estable 1.2)

--- SistemaDeMatriculas/Clases/ClasesNormales/Cconexion.cs
+++ SistemaDeMatriculas/Clases/ClasesNormales/Cconexion.cs
@@ -4,16 +4,13 @@ namespace SistemaDeMatriculas.Clases
 {
     public class Cconexion
     {
-        static string servidor = "tcp:bdproyectos.database.windows.net";
-        static string bd = "SistemaMatriculas";
-        static string user = "rarz";
-        static string pass = "exodus14@.#";
-
-        private string cconexion = $"Server={servidor},1433;Initial Catalog={bd};Persist Security Info=False;User ID={user};Password={pass};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private string cconexion = "Server=tcp:bdproyectos.database.windows.net,1433;Initial Catalog=SistemaMatriculas;Persist Security Info=False;User ID=rarz;Password=exodus14@.#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public SqlConnection ObtenerConexion()
         {
             return new SqlConnection(cconexion);
         }
     }
+
+
 }
